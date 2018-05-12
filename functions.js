df0b5664d0c0df657329caa809ccbe182ff3efb7
Anton <anton_123@inbox.ru>
2018-05-12 13:13:21 +0300

    коррекция кода

--- functions.js
+++ functions.js
@@ -6,11 +6,11 @@ $( function () {
 		for ( let i = 0; i < sex_radio.length; i++ ) {
 			let data_sex = $( 'div', '.sex-radio' )[ i ].getAttribute( 'data-sex' );
 			if ( sex === data_sex ) {
-				sex_radio[ i ].setAttribute( 'style', 'display: visible' );
-				users_item[ i ].setAttribute( 'style', 'display: visible' );
+				sex_radio[ i ].setAttribute( 'style', 'display: block' );
+				users_item[ i ].setAttribute( 'style', 'display: block' );
 			} else if (sex == 0) {
-				sex_radio[ i ].setAttribute( 'style', 'display: visible' );
-				users_item[ i ].setAttribute( 'style', 'display: visible' );
+				sex_radio[ i ].setAttribute( 'style', 'display: block' );
+				users_item[ i ].setAttribute( 'style', 'display: block' );
 			} else {
 				sex_radio[ i ].setAttribute( 'style', 'display: none' );
 				users_item[ i ].setAttribute( 'style', 'display: none' );
